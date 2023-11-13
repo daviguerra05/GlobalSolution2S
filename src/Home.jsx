4d1fa0fbdfaf63c7d61392270de78536c78a7df1
daviguerra05 <davi@passanha.com.br>
2023-11-13 16:15:19 -0300

    primeira base

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "./componentes/header/Header"
+import Footer from "./componentes/Footer/footer"
+
+export default function Home(){
+    return(
+        <>
+            <Header/>
+            <h1>HOME PAGE</h1>
+            <Footer/>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { } from 'react'
 import ReactDOM from 'react-dom/client'
+/* importando as dependencias do browserRouter e Router Provider*/
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+/* importando os elementos*/
 import App from './App.jsx'
-import './index.css'
+import Error from './Error.jsx'
+import Home from './Home.jsx'
+import './css/index.css'
+
+ /* criando a função createBrowser */
+ const router = createBrowserRouter([
+  {
+    /* chamando o elemento principal(pai) */
+    path: '/', element: <App />,
+
+    /*  chamando o elemento de erro*/
+    errorElement: <Error />,
+    /* chamando elementos filhos */
+    children:[
+      {path: '/', element:<Home/>},
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      {/*chamando a função que será renderizada */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
