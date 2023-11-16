31b334d2bc269526b4f24d34d311c4c986c73988
daviguerra05 <davi@passanha.com.br>
2023-11-16 18:34:01 -0300

    final

--- src/componentes/Footer/Footer.jsx
+++ src/componentes/Footer/Footer.jsx
@@ -3,7 +3,19 @@ import './footer.css'
 export default function Footer(){
     return(
         <>
-        <footer></footer>
+        <footer>
+            <div><div id='ft_logo'></div></div>
+            <div className='ft_'>
+                <h4>Nosso time</h4>
+                <a href="https://github.com/daviguerra05" target='_blank'>Davi Guerra</a>
+                <a href="https://github.com/ruiasiqueira" target='_blank'>Rui Amorim</a>
+            </div>
+            <div className='ft_'>
+                <h4>Contatos</h4>
+                <a href="">Davi Guerra</a>
+                <a href="">Rui Amorim</a>
+            </div>
+        </footer>
         </>
     )
 }
